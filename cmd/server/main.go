b12df01fefdcffa905bc364c53fcddea927c1220
krzysztofla <krzysztof.lach@icloud.com>
2022-06-21 14:43:03 +0200

    Cleaning repository

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,16 +1,5 @@
 package main
 
-import (
-	"net/http"
-
-	"github.com/gorilla/mux"
-	"github.com/krzysztofla/Example.Go.Api/router"
-)
-
 func main() {
-	mux_router := mux.NewRouter()
 
-	router.RegisterItemStoreRoutes(mux_router)
-
-	http.ListenAndServe("localhost:8080", mux_router)
 }
--- handlers/logger.go
+++
@@ -1,8 +0,0 @@
-package handler
-
-import (
-	"log"
-	"os"
-)
-
-var Logger = log.New(os.Stdout, "Basket-Api", log.LstdFlags)
